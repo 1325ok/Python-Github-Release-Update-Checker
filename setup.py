eb8ad0bf252d3d528f44d16b95366eee2c345cd1
1325ok <93064596+1325ok@users.noreply.github.com>
2025-04-05 14:33:34 +0900

    rename pkg name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="github-release-update-checker", # Replace with your own username
-    version="1.0.2",
+    version="1.0.3",
     author="1325OK",
     author_email="1325ok.help@gmail.com",
     description="A simple GUI-based update notifier in Python",
